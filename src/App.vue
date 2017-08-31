a48b319405febd9896aeccc403eac33729b6cde3
msazuredot <msazuredot@users.noreply.github.com>
2017-08-30 21:39:44 -0400

    Navigation menu added

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,12 @@
   <v-app light>
     <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
           <v-list-tile-action>
-            <v-icon left>account_balance</v-icon>
+            <v-icon left>{{item.icon}}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>Museum</v-list-tile-content>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
           </v-list-tile>
-          <v-list-tile>
-          <v-list-tile-action>
-            <v-icon left>trains</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>Trains</v-list-tile-content>
-          </v-list-tile>
-          <v-list-tile>
-          <v-list-tile-action>
-            <v-icon left>today</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>Events</v-list-tile-content>
-          </v-list-tile>
-        <!-- <v-list-tile><v-icon left>train</v-icon>Trains</v-list-tile> -->
-        <!-- <v-list-tile><v-icon left>today</v-icon>Events</v-list-tile> -->
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark class="primary">
@@ -31,13 +17,14 @@
       <v-toolbar-title>Choo-Choo-Trains</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat><v-icon left>account_balance</v-icon>Museums</v-btn>
-        <v-btn flat><v-icon left>train</v-icon>Trains</v-btn>
-        <v-btn flat><v-icon left>today</v-icon>Events</v-btn>
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{item.icon}}</v-icon>{{item.title}}</v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <main>
+      <router-view>
 
+      </router-view>
     </main>
   </v-app>
 </template>
@@ -47,6 +34,14 @@
     data() {
       return {
         sideNav: false,
+        menuItems: [
+          { icon: 'account_balance', title: 'Museums' },
+          { icon: 'train', title: 'Trains' },
+          { icon: 'today', title: 'Events' },
+          { icon: 'person', title: 'Profile' },
+          { icon: 'face', title: 'Sign Up' },
+          { icon: 'lock_open', title: 'Sign In' },
+        ],
       };
     },
   };
