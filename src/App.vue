266de7c9f08e2c25c12e910ab68dd9334f4bf391
msazuredot <msazuredot@users.noreply.github.com>
2017-08-31 21:43:30 -0400

    Small cleanup

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
         <v-list-tile
         v-for="item in menuItems"
         :key="item.title"
-        router
         :to="item.link">
           <v-list-tile-action>
             <v-icon left>{{item.icon}}</v-icon>
@@ -27,7 +26,6 @@
         <v-btn flat
         v-for="item in menuItems"
         :key="item.title"
-        router
         :to="item.link">
           <v-icon left>{{item.icon}}</v-icon>{{item.title}}</v-btn>
       </v-toolbar-items>
