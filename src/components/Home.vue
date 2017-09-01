124161fe39c92ef37391e1dd2ec730735f73e9da
msazuredot <msazuredot@users.noreply.github.com>
2017-08-31 21:38:34 -0400

    added link buttons to the home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <<template>
-  <div>
-    <p>The Home Page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+          <v-btn large router to="/events" class="accent"> See Events</v-btn>
+        </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+          <v-btn large router to="/events/new" class="accent">Add Event</v-btn>
+        </v-flex>
+    </v-layout>
+  </v-container>
 </template>
